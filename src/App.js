c6e385397165314973f956caaecaebf7c276bcf4
Roshan Shibu <roshanshibu97@gmail.com>
2023-09-13 23:26:38 +0200

    better colors

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import { Knob } from "react-rotary-knob";
 import * as skins from "react-rotary-knob-skin-pack";
 import { animated, useSpring } from "react-spring";
 import "./App.css";
+const red = "#CF2431";
+const orange = "#DA681D";
+const yellow = "#E9D514";
+const green = "#27CB3E";
+const blue = "#2EA1CE";
+const indigo = "#5682B8";
+const violet = "#8132B2";
 
 function App() {
   const [knobPos, setKnobPos] = useState(50);
   const [rainbow, setRainbow] = useState(
-    "violet,indigo,blue,green,orange,yellow,red,violet"
+    `${green}, ${green}, ${green}, ${green}, ${blue}, ${blue}, ${blue}, ${green}`
   );
   const animProps = useSpring({
     background: `conic-gradient(from 90deg,${rainbow})`,
@@ -16,17 +23,43 @@ function App() {
   const updateKnobPos = (newPos) => {
     console.log(`new position is  ${newPos}`);
     setKnobPos(newPos);
+    if (newPos > 10)
+      setRainbow(
+        `${red}, ${red}, ${red}, ${orange}, ${orange}, ${yellow}, ${yellow}, ${red}`
+      );
+    if (newPos > 20)
+      setRainbow(
+        `${orange}, ${orange}, ${yellow}, ${yellow}, ${yellow}, ${green}, ${green}, ${orange}`
+      );
+    if (newPos > 30)
+      setRainbow(
+        `${yellow}, ${green}, ${green}, ${green}, ${green}, ${blue}, ${blue}, ${yellow}`
+      );
+    if (newPos > 40)
+      setRainbow(
+        `${green}, ${green}, ${green}, ${green}, ${blue}, ${blue}, ${blue}, ${green}`
+      );
     if (newPos > 50)
-      setRainbow("violet,indigo,blue,green,orange,yellow,red,violet");
-    else {
-      if (newPos > 20)
-        setRainbow(
-          "#D02323, #EBD514, #0FCA43, #0BBBE1, #CE2777, #D02323, #D02323, #D02323"
-        );
-      else setRainbow("violet,green,green,green,green,green,green,violet");
-    }
+      setRainbow(
+        `${blue}, ${green}, ${green}, ${green}, ${blue}, ${blue}, ${blue}, ${blue}`
+      );
+    if (newPos > 60)
+      setRainbow(
+        `${blue}, ${blue}, ${blue}, ${indigo}, ${indigo}, ${indigo}, ${indigo}, ${blue}`
+      );
+    if (newPos > 70)
+      setRainbow(
+        `${indigo}, ${indigo}, ${indigo}, ${indigo}, ${violet}, ${violet}, ${violet}, ${indigo}`
+      );
+    if (newPos > 80)
+      setRainbow(
+        `${indigo}, ${indigo}, ${violet}, ${violet}, ${violet}, ${violet}, ${red}, ${indigo}`
+      );
+    if (newPos > 90)
+      setRainbow(
+        `${violet}, ${violet}, ${red}, ${red}, ${red}, ${orange}, ${red}, ${violet}`
+      );
   };
-
   return (
     <div className="App">
       <div className="knobAndRainbow">
